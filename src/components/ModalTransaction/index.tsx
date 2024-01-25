a9efd569258faa0bcabb93b592d45fddcc3e3a8b
Rafael <rafael.aguiar91@gmail.com>
2024-01-25 11:38:19 -0300

    feat: estrutura de funções do modal de transações

--- src/components/ModalTransaction/index.tsx
+++ src/components/ModalTransaction/index.tsx
@@ -1,6 +1,7 @@
 import './styles.scss';
 import { Dispatch, SetStateAction } from 'react';
 import { IoCloseCircleOutline } from "react-icons/io5";
+import { FormEvent, useState } from 'react';
 
 
 type ModalProps = {
@@ -9,22 +10,36 @@ type ModalProps = {
 }
 
 function ModalTransaction({ modal, setModal }: ModalProps) {
+  const [title, setTitle] = useState('');
+  const [amount, setAmount] = useState(0);
+  const [date, setDate] = useState('');
+
+  const handleTransaction = async (e: FormEvent) => {
+    e.preventDefault();
+  }
+
+  const handleClearForm = () => {
+    setTitle('');
+    setAmount(0);
+    setDate('');
+  }
+
   return (
     <div className="container-modal">
       <h2>Nova transação</h2>
       <div className='close-icon'>
-        <IoCloseCircleOutline size={25} onClick={()=>setModal(false)} />
+        <IoCloseCircleOutline size={25} onClick={() => setModal(false)} />
       </div>
-      <form>
+      <form onSubmit={handleTransaction}>
         <label htmlFor='title'>Título</label>
-        <input name='title' type='text' />
+        <input name='title' type='text' value={title} onChange={(e) => setTitle(e.target.value)} />
         <label htmlFor='value'>Valor</label>
-        <input name='value' type='number' />
+        <input name='value' type='number' value={amount} onChange={(e) => setAmount(Number(e.target.value))}/>
         <label htmlFor='date'>Data</label>
-        <input name='date' type='date' />
+        <input name='date' type='date' value={date} onChange={(e) => setDate(e.target.value)} />
         <div className='form-buttons'>
           <button className='green-btn'>Confirmar</button>
-          <button className='red-btn'>Cancelar</button>
+          <button type='button' className='red-btn' onClick={()=>handleClearForm()}>Cancelar</button>
         </div>
       </form>
     </div>

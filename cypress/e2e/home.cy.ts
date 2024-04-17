aa53703cc0dd01eb6aa17b7ac760a553cbad0c1b
Rafael <rafael.aguiar91@gmail.com>
2024-04-17 14:19:08 -0300

    test: melhorias nos testes da home

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,74 +1,64 @@
 describe('Tests the Home page', () => {
-  
+
     it('should show Home page items', () => {
-      cy.visit('/');
-      cy.get('[data-testid=input-username]').type("rafael");
-      cy.get('[data-testid=input-password]').type("123456");
-  
-      cy.intercept('POST', 'http://localhost:8000/login', {
-        fixture: 'login-response.json'
-      })
-  
-      cy.intercept('GET', 'http://localhost:8000/transactions', {
-        fixture: 'home-incomes'
-      })
-  
-      cy.contains('Confirmar').click();
-      cy.contains('Seja bem vindo');
-      cy.contains('Entradas');
-      cy.contains('Saídas');
-      cy.contains('Total');
 
-      cy.contains('Título');
-      cy.contains('Valor');
-      cy.contains('Data');
+        cy.window().then((window) => {
+            window.localStorage.setItem('token', 'fakeToken');
+        });
 
-      cy.contains('Nova transação');
+        cy.intercept('GET', 'http://localhost:8000/transactions', {
+            fixture: 'home-incomes'
+        })
 
-      cy.contains('Sair');
+        cy.visit('/home');
+        cy.contains('Seja bem vindo');
+        cy.contains('Entradas');
+        cy.contains('Saídas');
+        cy.contains('Total');
+        cy.contains('Título');
+        cy.contains('Valor');
+        cy.contains('Data');
+
+        cy.contains('Nova transação');
+        cy.contains('Sair');
     });
 
     it('should open a modal for transactions when clicks on "Nova transação"', () => {
-        cy.visit('/');
-        cy.get('[data-testid=input-username]').type("rafael");
-        cy.get('[data-testid=input-password]').type("123456");
-    
-        cy.intercept('POST', 'http://localhost:8000/login', {
-          fixture: 'login-response.json'
-        })
-    
-        cy.intercept('GET', 'http://localhost:8000/transactions', {
-          fixture: 'home-incomes'
-        })
-    
-        cy.contains('Confirmar').click();
 
+        cy.window().then((window) => {
+            window.localStorage.setItem('token', 'fakeToken');
+        });
+
+         cy.intercept('GET', 'http://localhost:8000/transactions', {
+           fixture: 'home-incomes'
+         })
+
+        cy.visit('/home');
         cy.contains('Nova transação').click();
         cy.contains('Entrada');
         cy.contains('Saída');
         cy.contains('Confirmar');
         cy.contains('Cancelar');
-      });
+    });
 
-      it('should go back to Login page when clicks on "Sair"', () => {
+    it('should go back to Login page when clicks on "Sair"', () => {
         cy.visit('/');
         cy.get('[data-testid=input-username]').type("rafael");
         cy.get('[data-testid=input-password]').type("123456");
-    
+
         cy.intercept('POST', 'http://localhost:8000/login', {
-          fixture: 'login-response.json'
+            fixture: 'login-response.json'
         })
-    
+
         cy.intercept('GET', 'http://localhost:8000/transactions', {
-          fixture: 'home-incomes'
+            fixture: 'home-incomes'
         })
-    
+
         cy.contains('Confirmar').click();
 
         cy.contains('Sair').click();
         cy.contains('Login');
-      })
-      
-  })
-  
-  
+    })
+
+})
+
